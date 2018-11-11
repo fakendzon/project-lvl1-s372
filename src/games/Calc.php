e833443b947b9b9eb3f44e7ed4f52e9998455df7
Potapov <mihal_palich.93@mail.ru>
2018-11-11 12:05:43 +0700

    fix codestyle

--- src/games/Calc.php
+++ src/games/Calc.php
@@ -5,6 +5,7 @@ namespace BrainGames\Games\Calc;
 use function BrainGames\Cli\game;
 
 const DESCRIPTION = "What is the result of the expression?";
+const OPERATIONS  = ['-' => 'minus', '+' => 'plus', '*' => 'multiplication'];
 
 function run()
 {
@@ -12,24 +13,27 @@ function run()
         $num1      = rand(1, 10);
         $num2      = rand(1, 10);
 
-        $operations = [
-            '-' => function ($num1, $num2) {
-                return $num1 - $num2;
-            },
-            '+' => function ($num1, $num2) {
-                return $num1 + $num2;
-            },
-            '*' => function ($num1, $num2) {
-                return $num1 * $num2;
-            },
-        ];
-
-        $previewOperations = array_keys($operations);
+        $previewOperations = array_keys(OPERATIONS);
         $operation = $previewOperations[array_rand($previewOperations)];
         $question  = "{$num1} {$operation} {$num2}";
 
-        return [$question, $operations[$operation]($num1, $num2)];
+        return [$question, call_user_func(__NAMESPACE__.'\\'.OPERATIONS[$operation], $num1, $num2)];
     };
 
     game($generateQuestionAndAnswer, DESCRIPTION);
+}
+
+function minus($num1, $num2)
+{
+    return $num1 - $num2;
+}
+
+function plus($num1, $num2)
+{
+    return $num1 + $num2;
+}
+
+function multiplication($num1, $num2)
+{
+    return $num1 * $num2;
 }
